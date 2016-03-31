184f7cb7ee30f2d64d7be87ec3ea6786cda45d8e
MikeKnowles <michael.knowles@inspection.gc.ca>
2016-03-31 14:47:46 -0400

    Removing Busco

--- accessoryfiles.sh
+++ accessoryfiles.sh
@@ -21,7 +21,6 @@ files=( http://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.5.
         http://downloads.sourceforge.net/project/bbmap/BBMap_35.82.tar.gz
         http://spades.bioinf.spbau.ru/release3.6.2/SPAdes-3.6.2-Linux.tar.gz
         https://downloads.sourceforge.net/project/quast/quast-3.2.tar.gz
-        http://busco.ezlab.org/files/BUSCO_v1.1b1.tar.gz
         https://github.com/samtools/samtools/releases/download/1.3/samtools-1.3.tar.bz2
         https://bitbucket.org/kokonech/qualimap/downloads/qualimap_v2.2.zip
         https://github.com/BenLangmead/bowtie2/releases/download/v2.2.8/bowtie2-2.2.8-linux-x86_64.zip
@@ -30,11 +29,11 @@ files=( http://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.5.
 
 echo ${files[@]} | xargs -n 1 -P 8 wget -q -P ${PREFIX}
 
-for clade in bacteria eukaryota fungi; do
-    wget http://busco.ezlab.org/files/${clade}_buscos.tar.gz -P ${PREFIX};
-    tar -zxf ${PREFIX}/${clade}_buscos.tar.gz -C ${PREFIX};
-    rm ${PREFIX}/${clade}_buscos.tar.gz;
-done
+# for clade in bacteria eukaryota fungi; do
+#     wget http://busco.ezlab.org/files/${clade}_buscos.tar.gz -P ${PREFIX};
+#     tar -zxf ${PREFIX}/${clade}_buscos.tar.gz -C ${PREFIX};
+#     rm ${PREFIX}/${clade}_buscos.tar.gz;
+# done
 
 for a in $(ls -1 ${PREFIX}/*.tar.gz); do
 tar -zxf $a -C $PREFIX;
